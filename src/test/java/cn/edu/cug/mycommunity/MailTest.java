c34e86c00c8fbb3c18355c6b0b48fbd935b6e6c8
cxz0214 <40239400+cxz0214@users.noreply.github.com>
2020-12-10 10:43:21 +0800

    更新

---
+++ src/test/java/cn/edu/cug/mycommunity/MailTest.java
@@ -0,0 +1,35 @@
+package cn.edu.cug.mycommunity;
+
+
+import cn.edu.cug.mycommunity.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@ContextConfiguration(classes = MycommunityApplication.class)
+public class MailTest {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("1721529255@qq.com","Hello World","加油，你可以的！");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","sunday");
+
+        String content = templateEngine.process("/mail/demo",context);
+        System.out.println(content);
+        mailClient.sendMail("1721529255@qq.com","Html",content);
+    }
+}
